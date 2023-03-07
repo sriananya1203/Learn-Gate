067e589ee54fb5d29c7cfdd4ded781b609c39f93
AnanyaSrivastava1203 <sriananya1203@gmail.com>
2023-03-07 22:45:25 +0530

    another pattern

---
+++ practice_pattern.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pi 3.14
+#define mod 1000000007
+#define nl '\n'
+#define int long long int
+const int N = 1e5 + 10;
+
+signed main(){
+    int i,j;
+    for(i=1;i<=5;i++)
+    {
+        for(int j=1;j<=5;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
